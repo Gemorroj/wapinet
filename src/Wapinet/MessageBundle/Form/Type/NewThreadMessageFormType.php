8ef8e3dbdd276b30e0547396cd03f85386e38889
Gemorroj <wapinet@gmail.com>
2013-11-03 22:29:47 +0300

    личные сообщения

---
+++ src/Wapinet/MessageBundle/Form/Type/NewThreadMessageFormType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Wapinet\MessageBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use FOS\MessageBundle\FormType\NewThreadMessageFormType as BaseType;
+
+/**
+ * Message form type for starting a new conversation
+ *
+ * @author Thibault Duplessis <thibault.duplessis@gmail.com>
+ */
+class NewThreadMessageFormType extends BaseType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        $builder
+            ->add('recipient', 'fos_user_username', array('label' => 'Получатель'))
+            ->add('subject', 'text', array('label' => 'Тема'))
+            ->add('body', 'textarea', array('label' => 'Сообщение'));
+    }
+
+    public function getName()
+    {
+        return 'wapinet_message_new_thread';
+    }
+}
---
+++ src/Wapinet/MessageBundle/Form/Type/ReplyMessageFormType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Wapinet\MessageBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use FOS\MessageBundle\FormType\ReplyMessageFormType as BaseType;
+
+/**
+ * Form type for a reply
+ *
+ * @author Thibault Duplessis <thibault.duplessis@gmail.com>
+ */
+class ReplyMessageFormType extends BaseType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+        $builder
+            ->add('body', 'textarea', array('label' => 'Сообщение'));
+    }
+
+    public function getName()
+    {
+        return 'wapinet_message_reply_message';
+    }
+}
