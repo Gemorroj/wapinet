a681a7399ce669feda9a3be5ca3fe33267a49646
Gemorroj <wapinet@gmail.com>
2013-11-03 23:53:50 +0300

    просмотр профилей пользователей

--- src/Wapinet/UserBundle/Controller/ProfileController.php
+++ src/Wapinet/UserBundle/Controller/ProfileController.php
@@ -1,10 +1,30 @@
 <?php
 namespace Wapinet\UserBundle\Controller;
 
+use FOS\UserBundle\Model\UserInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use FOS\UserBundle\Controller\ProfileController as BaseController;
 
 
 class ProfileController extends BaseController
 {
+    /**
+     * Show the user
+     * @param int $id
+     */
+    public function showAction($id = null)
+    {
+        $currentUser = $this->container->get('security.context')->getToken()->getUser();
+        if (!is_object($currentUser) || !$currentUser instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+        if (null !== $id) {
+            $userManager = $this->container->get('fos_user.user_manager');
+            $user = $userManager->findUserBy(array('id' => $id));
+        } else {
+            $user = $currentUser;
+        }
 
+        return $this->container->get('templating')->renderResponse('FOSUserBundle:Profile:show.html.'.$this->container->getParameter('fos_user.template.engine'), array('user' => $user));
+    }
 }
