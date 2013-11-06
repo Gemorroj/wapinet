416cc2ab1f3a673111d8df5b55708f8fda62359a
Gemorroj <wapinet@gmail.com>
2013-11-06 16:04:04 +0300

    phpdoc

--- src/Wapinet/MessageBundle/Provider/Provider.php
+++ src/Wapinet/MessageBundle/Provider/Provider.php
@@ -4,6 +4,7 @@ namespace Wapinet\MessageBundle\Provider;
 
 use FOS\MessageBundle\Provider\Provider as BaseProvider;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Pagerfanta\Pagerfanta;
 
 /**
  * Provides threads for the current authenticated user
@@ -21,7 +22,7 @@ class Provider extends BaseProvider
      * Gets the thread in the inbox of the current user
      *
      * @param int $page
-     * @return array of ThreadInterface
+     * @return Pagerfanta
      */
     public function getInboxThreads($page = 1)
     {
@@ -37,7 +38,7 @@ class Provider extends BaseProvider
      * Gets the thread in the sentbox of the current user
      *
      * @param int $page
-     * @return array of ThreadInterface
+     * @return Pagerfanta
      */
     public function getSentThreads($page = 1)
     {
@@ -53,7 +54,7 @@ class Provider extends BaseProvider
      * Gets the thread in the deleted of the current user
      *
      * @param int $page
-     * @return array of ThreadInterface
+     * @return Pagerfanta
      */
     public function getDeletedThreads($page = 1)
     {
