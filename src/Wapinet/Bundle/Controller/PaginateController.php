a61b25ee07cf30821deed0f5c2821f69ae1e3ea7
Gemorroj <wapinet@gmail.com>
2013-11-07 11:23:16 +0300

    пагинация в личных сообщениях

--- src/Wapinet/Bundle/Controller/PaginateController.php
+++ src/Wapinet/Bundle/Controller/PaginateController.php
@@ -3,20 +3,34 @@
 namespace Wapinet\Bundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Query;
+use Doctrine\ORM\QueryBuilder;
 use Pagerfanta\Adapter\DoctrineORMAdapter;
+use Pagerfanta\Adapter\DoctrineCollectionAdapter;
 use Pagerfanta\Pagerfanta;
+
 
 class PaginateController extends Controller
 {
     /**
-     * @param \Doctrine\ORM\Query|\Doctrine\ORM\QueryBuilder $query
+     * @param Query|QueryBuilder|Collection $data
      * @param int $page
      *
+     * @throws \RuntimeException
      * @return Pagerfanta
      */
-    public function paginate($query, $page = 1)
+    public function paginate($data, $page = 1)
     {
-        $pagerfanta = new Pagerfanta(new DoctrineORMAdapter($query));
+        if ($data instanceof Collection) {
+            $adapter = new DoctrineCollectionAdapter($data);
+        } elseif ($data instanceof Query || $data instanceof QueryBuilder) {
+            $adapter = new DoctrineORMAdapter($data);
+        } else {
+            throw new \RuntimeException('Неизвестный тип данных для постраничной навигации');
+        }
+
+        $pagerfanta = new Pagerfanta($adapter);
         $pagerfanta->setMaxPerPage($this->container->getParameter('wapinet_paginate_maxperpage'));
         $pagerfanta->setCurrentPage($page);
 
--- src/Wapinet/MessageBundle/Controller/MessageController.php
+++ src/Wapinet/MessageBundle/Controller/MessageController.php
@@ -4,6 +4,7 @@ namespace Wapinet\MessageBundle\Controller;
 
 use FOS\MessageBundle\Controller\MessageController as BaseController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 
 class MessageController extends BaseController
@@ -18,7 +19,7 @@ class MessageController extends BaseController
     {
         $threads = $this->getProvider()->getInboxThreads($page);
 
-        return $this->container->get('templating')->renderResponse('FOSMessageBundle:Message:inbox.html.twig', array(
+        return $this->container->get('templating')->renderResponse('WapinetMessageBundle:Message:inbox.html.twig', array(
             'threads' => $threads
         ));
     }
@@ -33,7 +34,7 @@ class MessageController extends BaseController
     {
         $threads = $this->getProvider()->getSentThreads($page);
 
-        return $this->container->get('templating')->renderResponse('FOSMessageBundle:Message:sent.html.twig', array(
+        return $this->container->get('templating')->renderResponse('WapinetMessageBundle:Message:sent.html.twig', array(
             'threads' => $threads
         ));
     }
@@ -48,8 +49,40 @@ class MessageController extends BaseController
     {
         $threads = $this->getProvider()->getDeletedThreads($page);
 
-        return $this->container->get('templating')->renderResponse('FOSMessageBundle:Message:deleted.html.twig', array(
+        return $this->container->get('templating')->renderResponse('WapinetMessageBundle:Message:deleted.html.twig', array(
             'threads' => $threads
+        ));
+    }
+
+    /**
+     * Displays a thread, also allows to reply to it
+     *
+     * @param string $threadId the thread id
+     * @param int $page
+     *
+     * @return Response
+     */
+    public function threadAction($threadId, $page = 1)
+    {
+        /** @var \Wapinet\MessageBundle\Entity\Thread $thread */
+        $thread = $this->getProvider()->getThread($threadId);
+
+        $messages = $this->container->get('wapinet.paginate.controller')->paginate($thread->getMessages(), $page);
+
+
+        $form = $this->container->get('fos_message.reply_form.factory')->create($thread);
+        $formHandler = $this->container->get('fos_message.reply_form.handler');
+
+        if ($message = $formHandler->process($form)) {
+            return new RedirectResponse($this->container->get('router')->generate('fos_message_thread_view', array(
+                'threadId' => $message->getThread()->getId()
+            )));
+        }
+
+        return $this->container->get('templating')->renderResponse('WapinetMessageBundle:Message:thread.html.twig', array(
+            'messages' => $messages,
+            'form' => $form->createView(),
+            'thread' => $thread
         ));
     }
 }
