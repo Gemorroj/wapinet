063714d162903f8b153af728beb2ed8495885199
Gemorroj <wapinet@gmail.com>
2013-11-16 14:28:20 +0200

    переделано меню

--- src/Wapinet/Bundle/Twig/Extension/Menu.php
+++ src/Wapinet/Bundle/Twig/Extension/Menu.php
@@ -2,8 +2,20 @@
 
 namespace Wapinet\Bundle\Twig\Extension;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
 class Menu extends \Twig_Extension
 {
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
     /**
      * Returns a list of global functions to add to the existing list.
      *
@@ -20,22 +32,22 @@ class Menu extends \Twig_Extension
      * @param array $options
      * @return string
      */
-    public function getMenu(array $options = array(array('uri' => '/', 'title' => 'wapinet.ru')))
+    public function getMenu(array $options = array())
     {
-        $out = '<div data-role="controlgroup" data-type="horizontal" data-mini="true" class="ui-btn-left">';
+        $out = '';
+
+        $out .= '<div data-role="controlgroup" data-type="horizontal" data-mini="true" class="ui-btn-left">';
         //$out .= '<a href="javascript:history.back();" data-role="button" data-icon="back" data-iconpos="notext">Назад</a>';
         //$out .= '<a href="#menu" data-role="button" data-icon="bars">Меню</a>';
-        $out .= '<a href="#menu" data-role="button" data-icon="bars" data-iconpos="notext">Меню</a>';
+        //$out .= '<a href="#menu" data-role="button" data-icon="bars" data-iconpos="notext">Меню</a>';
+        $out .= '<a data-role="button" data-iconpos="notext" data-icon="home" href="' . $this->container->get('router')->generate('index') . '">Главная</a>';
 
         ksort($options, SORT_NUMERIC);
-        $last = array_pop($options);
-
         foreach ($options as $value) {
             $out .= '<a data-role="button" data-icon="arrow-l" href="' . htmlspecialchars($value['uri']) . '">' . htmlspecialchars($value['title'], ENT_NOQUOTES) . '</a>';
         }
 
         $out .= '</div>';
-        $out .= '<h2>' . htmlspecialchars($last['title'], ENT_NOQUOTES) . '</h2>';
 
         return $out;
     }
