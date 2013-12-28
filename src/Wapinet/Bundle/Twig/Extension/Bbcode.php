3e80cc49ed8de2b7409fb2ace9449802afcdb25e
Gemorroj <wapinet@gmail.com>
2013-12-28 17:10:20 +0200

    добавлена поддержка ббкода

---
+++ src/Wapinet/Bundle/Twig/Extension/Bbcode.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Wapinet\Bundle\Twig\Extension;
+
+use Xbbcode\Xbbcode;
+
+class Bbcode extends \Twig_Extension
+{
+    /**
+     * Returns a list of global functions to add to the existing list.
+     *
+     * @return array An array of global functions
+     */
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter('wapinet_bbcode_parse', array($this, 'bbcodeParse'), array('is_safe' => array('html'))),
+        );
+    }
+
+
+    /**
+     * @param string $text
+     * @return string
+     */
+    public function bbcodeParse($text)
+    {
+        $xbbcode = new Xbbcode($text);
+
+        return $xbbcode->get_html();
+    }
+
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     */
+    public function getName()
+    {
+        return 'wapinet_bbcode';
+    }
+}
