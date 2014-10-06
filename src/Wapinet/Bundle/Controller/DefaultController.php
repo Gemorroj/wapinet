dcac562bad340bfb87a2c755e1006a6c990f3ed5
Gemorroj <wapinet@gmail.com>
2014-10-06 11:31:58 +0300

    добавлен контроль закачек сеа

--- src/Wapinet/Bundle/Controller/DefaultController.php
+++ src/Wapinet/Bundle/Controller/DefaultController.php
@@ -3,19 +3,32 @@
 namespace Wapinet\Bundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Wapinet\Bundle\Entity\Sea;
 
 class DefaultController extends Controller
 {
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function indexAction()
     {
         return $this->render('WapinetBundle:Default:index.html.twig');
     }
 
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function aboutAction()
     {
         return $this->render('WapinetBundle:Default:about.html.twig');
     }
 
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function onlineAction()
     {
         return $this->render(
@@ -26,38 +39,87 @@ class DefaultController extends Controller
         );
     }
 
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function utilitiesAction()
     {
         return $this->render('WapinetBundle:Default:utilities.html.twig');
     }
 
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function programmingAction()
     {
         return $this->render('WapinetBundle:Default:programming.html.twig');
     }
 
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function openSourceAction()
     {
         return $this->render('WapinetBundle:Default:open_source.html.twig');
     }
 
+    /**
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
     public function gmanagerAction()
     {
         return $this->redirect('https://github.com/Gemorroj/gmanager', 301);
     }
 
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function downloadsAction()
     {
         return $this->render('WapinetBundle:Default:downloads.html.twig');
     }
 
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function buyPhpScriptsAction()
     {
         return $this->render('WapinetBundle:Default:buy_php_scripts.html.twig');
     }
 
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function textbookAction()
     {
         return $this->render('WapinetBundle:Default:textbook.html.twig');
+    }
+
+    /**
+     * @param Request $request
+     * @return BinaryFileResponse
+     */
+    public function seaDownloadAction(Request $request)
+    {
+        $seaCustomer = new Sea();
+        $seaCustomer->setDatetime(new \DateTime());
+        $seaCustomer->setIp($request->getClientIp());
+        $seaCustomer->setBrowser($request->headers->get('User-Agent', ''));
+        $seaCustomer->setReferer($request->headers->get('Referer'));
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($seaCustomer);
+        $em->flush();
+
+        $file = $this->container->getParameter('wapinet_sea_file');
+
+        $response = new BinaryFileResponse($file);
+        $response->setPrivate();
+        $response->setContentDisposition(
+            ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+            'sea_for_customers.zip'
+        );
+
+        return $response;
     }
 }
---
+++ src/Wapinet/Bundle/Entity/Sea.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace Wapinet\Bundle\Entity;
+
+
+/**
+ * Sea
+ */
+class Sea
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     */
+    private $datetime;
+
+    /**
+     * @var string
+     */
+    private $ip;
+
+    /**
+     * @var string
+     */
+    private $browser;
+
+    /**
+     * @var string
+     */
+    private $referer;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set id
+     *
+     * @param int $id
+     * @return Sea
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+
+    /**
+     * Set datetime
+     *
+     * @param \DateTime $datetime
+     * @return Sea
+     */
+    public function setDatetime(\DateTime $datetime)
+    {
+        $this->datetime = $datetime;
+
+        return $this;
+    }
+
+    /**
+     * Get datetime
+     *
+     * @return \DateTime
+     */
+    public function getDatetime()
+    {
+        return $this->datetime;
+    }
+
+    /**
+     * Set ip
+     *
+     * @param string $ip
+     * @return Sea
+     */
+    public function setIp($ip)
+    {
+        $this->ip = $ip;
+
+        return $this;
+    }
+
+    /**
+     * Get ip
+     *
+     * @return string
+     */
+    public function getIp()
+    {
+        return $this->ip;
+    }
+
+    /**
+     * Set browser
+     *
+     * @param string $browser
+     * @return Sea
+     */
+    public function setBrowser($browser)
+    {
+        $this->browser = $browser;
+
+        return $this;
+    }
+
+    /**
+     * Get browser
+     *
+     * @return string
+     */
+    public function getBrowser()
+    {
+        return $this->browser;
+    }
+
+
+    /**
+     * Set referer
+     *
+     * @param string $referer
+     * @return Sea
+     */
+    public function setReferer($referer)
+    {
+        $this->referer = $referer;
+
+        return $this;
+    }
+
+    /**
+     * Get referer
+     *
+     * @return string
+     */
+    public function getReferer()
+    {
+        return $this->referer;
+    }
+}
