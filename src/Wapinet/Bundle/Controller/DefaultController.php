5990dc799f33860f9adcf277e6e3af02d090daea
Gemorroj <wapinet@gmail.com>
2014-10-10 17:29:10 +0300

    Добавлена страница с отображением комитов в репозиторий

--- src/Wapinet/Bundle/Controller/DefaultController.php
+++ src/Wapinet/Bundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\Process\ProcessBuilder;
 use Wapinet\Bundle\Entity\Sea;
 
 class DefaultController extends Controller
@@ -24,6 +25,59 @@ class DefaultController extends Controller
     public function aboutAction()
     {
         return $this->render('WapinetBundle:Default:about.html.twig');
+    }
+
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function logAction()
+    {
+        $processBuilder = new ProcessBuilder();
+        $processBuilder->setPrefix(
+            $this->container->getParameter('wapinet_hg_path')
+        );
+        $processBuilder->setArguments(array(
+            'log',
+            '-l ' . $this->container->getParameter('wapinet_hg_log')
+        ));
+        $output = $processBuilder->getProcess()->mustRun()->getOutput();
+        //$output = iconv('cp1251', 'utf-8', $output); // windows only
+
+        return $this->render('WapinetBundle:Default:log.html.twig', array('log' => $this->formatLog($output)));
+    }
+
+    /**
+     * Преобразовываем строковый вывод консоли в массив
+     *
+     * @param string $output
+     * @return array
+     */
+    private function formatLog($output)
+    {
+        $result = array();
+        $output = rtrim($output);
+        $changes = explode("\n\n", $output);
+        $timezone = $this->get('timezone')->getTimezone();
+
+        foreach ($changes as $change) {
+            $items = explode("\n", $change);
+
+            $tmpResult = array();
+            foreach ($items as $item) {
+                list($key, $value) = explode(':', $item, 2);
+
+                if ('date' === $key) {
+                    $tmpResult[$key] = new \DateTime(ltrim($value), $timezone);
+                } else {
+                    $tmpResult[$key] = ltrim($value);
+                }
+            }
+
+            $result[] = $tmpResult;
+        }
+
+        return $result;
     }
 
     /**
