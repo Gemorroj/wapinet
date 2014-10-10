47edfee4d1181275d509e86160f54e37a93049b7
Gemorroj <wapinet@gmail.com>
2014-10-10 16:12:45 +0300

    удалены неиспользуемые css классы в пагинаторе

--- src/Wapinet/Bundle/Pagerfanta/Template.php
+++ src/Wapinet/Bundle/Pagerfanta/Template.php
@@ -9,9 +9,9 @@ class Template extends DefaultTemplate
     static protected $defaultOptions = array(
         'previous_message'   => 'Пред.',
         'next_message'       => 'След.',
-        'css_disabled_class' => 'disabled',
-        'css_dots_class'     => 'dots',
-        'css_current_class'  => 'current',
+        'css_disabled_class' => '',
+        'css_dots_class'     => '',
+        'css_current_class'  => '',
         'dots_text'          => '...',
         'container_template' => '<nav data-role="controlgroup" data-type="horizontal">%pages%</nav>',
         'page_template'      => '<a href="%href%" data-role="button">%text%</a>',
