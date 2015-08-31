9d25da041340a2c430fbf94318a875acc7c000eb
Gemorroj <wapinet@gmail.com>
2015-08-31 15:35:34 +0300

    fix transliterator 2

--- src/Wapinet/Bundle/Helper/Translit.php
+++ src/Wapinet/Bundle/Helper/Translit.php
@@ -12,6 +12,7 @@ class Translit
      */
     public function toAscii($str)
     {
-        return \transliterator_transliterate('Any-Latin; Latin-ASCII; [\u0100-\u7fff] remove', $str);
+        // return \transliterator_transliterate('Any-Latin; Latin-ASCII; [\u0100-\u7fff] remove', $str);
+        return \iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE', $str);
     }
 }
