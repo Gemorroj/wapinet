fd79b5b56884ca0e9ceb4268d6bf3b0b2d1a1add
Gemorroj <wapinet@gmail.com>
2016-01-09 20:58:02 +0000

    reindex.bat удалён онлайн на Bitbucket

--- bin/sphinx/reindex.bat
+++
@@ -1,2 +0,0 @@
-s:\sphinx\bin\indexer --rotate --config s:\OpenServer\domains\wapinet-new.loc\app\config\sphinx.conf files
-s:\sphinx\bin\indexer --rotate --config s:\OpenServer\domains\wapinet-new.loc\app\config\sphinx.conf users
