05d08614ed8ba7b104f1896b874c3907c77160b1
Gemorroj <wapinet@gmail.com>
2016-07-03 11:47:59 +0300

    убран google pr из seo анализа (google прекратила его поддержку), добавлена информация по ссылкам fcebook и vk

--- src/Wapinet/Bundle/Controller/PagerankController.php
+++ src/Wapinet/Bundle/Controller/PagerankController.php
@@ -2,6 +2,8 @@
 
 namespace Wapinet\Bundle\Controller;
 
+use SEOstats\Services\Google;
+use SEOstats\Services\Social;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Wapinet\Bundle\Form\Type\Pagerank\PagerankType;
@@ -45,30 +47,36 @@ class PagerankController extends Controller
      */
     protected function getPagerank(array $data)
     {
-        $url = \str_ireplace('http://', '', $data['url']);
+        $url = $data['url'];
+        $domain = \str_ireplace(array('http://', 'https://'), '', $url);
 
         return array(
-            'url' => $url,
-            'google_pages' => $this->getGooglePages($url),
-            'google_inurl' => $this->getGoogleInurl($url),
-            'google_pr' => $this->getGooglePr($url),
-            'yandex_pages' => $this->getYandexPages($url),
-            'yandex_tcy' => $this->getYandexTcy($url),
+            'domain' => $domain,
+            'google' => array(
+                'pages' => $this->getGooglePages($domain),
+                'inurl' => $this->getGoogleInurl($domain),
+            ),
+            'yandex' => array(
+                'pages' => $this->getYandexPages($domain),
+                'tcy' => $this->getYandexTcy($domain),
+            ),
+            'facebook' => Social::getFacebookShares($url),
+            'vk' => Social::getVKontakteShares($url),
         );
     }
 
     /**
-     * @param string $url
+     * @param string $domain
      * @return string
      */
-    protected function getYandexPages($url)
+    private function getYandexPages($domain)
     {
         $curl = $this->get('curl');
         $curl->init('https://yandex.ru/search/xml?user=gemorwapinet&key=' . $this->container->getParameter('wapinet_search_key'));
         $curl->setOpt(\CURLOPT_POST, true);
-        $curl->setOpt(\CURLOPT_POSTFIELDS, '<?xml version="1.0" encoding="UTF-8"?><request><query>' . \htmlspecialchars($url, \ENT_XML1) . '</query><groupings><groupby groups-on-page="1"/></groupings></request>');
+        $curl->setOpt(\CURLOPT_POSTFIELDS, '<?xml version="1.0" encoding="UTF-8"?><request><query>' . \htmlspecialchars($domain, \ENT_XML1) . '</query><groupings><groupby groups-on-page="1"/></groupings></request>');
 
-        $out = 'N/A';
+        $out = 'n.a.';
         try {
             $response = $curl->exec();
 
@@ -82,16 +90,16 @@ class PagerankController extends Controller
 
 
     /**
-     * @param string $url
+     * @param string $domain
      * @return string
      */
-    protected function getYandexTcy($url)
+    private function getYandexTcy($domain)
     {
         $curl = $this->get('curl');
-        $curl->init('https://bar-navig.yandex.ru/u?ver=2&show=32&url=http://' . $url);
+        $curl->init('https://bar-navig.yandex.ru/u?ver=2&show=32&url=http://' . $domain);
         $curl->addCompression();
 
-        $out = 'N/A';
+        $out = 'n.a.';
         try {
             $response = $curl->exec();
 
@@ -105,18 +113,20 @@ class PagerankController extends Controller
 
 
     /**
-     * @param string $url
+     * @param string $domain
      * @return string
      */
-    protected function getGooglePages($url)
+    protected function getGooglePages($domain)
     {
+        //return Google::getSiteindexTotal($domain);
+
         $curl = $this->get('curl');
-        $curl->init('http://www.google.com/search?hl=en&q=' . \rawurlencode('site:' . $url));
+        $curl->init('https://www.google.com/search?hl=en&q=' . \rawurlencode('site:' . $domain));
         $curl->acceptRedirects();
         $curl->addCompression();
         $curl->addHeader('Accept-Language', 'en-US,en');
 
-        $out = 'N/A';
+        $out = 'n.a.';
         try {
             $response = $curl->exec();
             /*
@@ -146,18 +156,20 @@ class PagerankController extends Controller
 
 
     /**
-     * @param string $url
+     * @param string $domain
      * @return string
      */
-    protected function getGoogleInurl($url)
+    protected function getGoogleInurl($domain)
     {
+        //return Google::getBacklinksTotal($domain);
+
         $curl = $this->get('curl');
-        $curl->init('http://www.google.com/search?hl=en&q=' . \rawurlencode('"' . $url . '" -inurl:"' . $url . '"'));
+        $curl->init('https://www.google.com/search?hl=en&q=' . \rawurlencode('"' . $domain . '" -inurl:"' . $domain . '"'));
         $curl->acceptRedirects();
         $curl->addCompression();
         $curl->addHeader('Accept-Language', 'en-US,en');
 
-        $out = 'N/A';
+        $out = 'n.a.';
         try {
             $response = $curl->exec();
             /*
@@ -184,35 +196,5 @@ class PagerankController extends Controller
         } catch (\Exception $e) {}
 
         return $out;
-    }
-
-
-
-    /**
-     * PageRank Lookup (Based on Google Toolbar for Mozilla Firefox)
-     *
-     * @copyright   2012 HM2K <hm2k@php.net>
-     * @link        http://pagerank.phurix.net/
-     * @author      James Wade <hm2k@php.net>
-     * @version     $Revision: 2.1 $
-     * @require     PHP 4.3.0 (file_get_contents)
-     * @updated		06/10/11
-     * @see         https://github.com/neatstudio/pagerank
-     */
-    protected function getGooglePr($q, $host = 'toolbarqueries.google.com', $context = null)
-    {
-        $seed = "Mining PageRank is AGAINST GOOGLE'S TERMS OF SERVICE. Yes, I'm talking to you, scammer.";
-        $result = 0x01020345;
-        $len = strlen($q);
-        for ($i=0; $i<$len; $i++) {
-            $result ^= ord($seed{$i%strlen($seed)}) ^ ord($q{$i});
-            $result = (($result >> 23) & 0x1ff) | $result << 9;
-        }
-        if (PHP_INT_MAX != 2147483647) { $result = -(~($result & 0xFFFFFFFF) + 1); }
-        $ch=sprintf('8%x', $result);
-        $url='http://%s/tbr?client=navclient-auto&ch=%s&features=Rank&q=info:%s';
-        $url=sprintf($url,$host,$ch,$q);
-        @$pr=file_get_contents($url,false,$context);
-        return $pr?substr(strrchr($pr, ':'), 1):false;
     }
 }
