61ddd83caf5f9c920649b94a2137b98b38a7c4e5
Gemorroj <wapinet@gmail.com>
2016-07-18 09:51:16 +0300

    add test extract rar archive

--- tests/Wapinet/Bundle/Controller/ArchiverControllerTest.php
+++ tests/Wapinet/Bundle/Controller/ArchiverControllerTest.php
@@ -14,4 +14,29 @@ class ArchiverControllerTest extends WebTestCaseWapinet
         self::assertEquals(200, $client->getResponse()->getStatusCode());
     }
 
+
+    public function testExtract()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/archiver/extract');
+
+        $form = $crawler->filter('form[name="archiver_add"]')->form();
+
+        $form['archiver_add[file][file]']->upload($this->getFixturesPath().'/sample.rar');
+
+        $client->submit($form);
+
+
+        $this->assertTrue($client->getResponse()->isRedirection());
+
+        $crawler = $client->followRedirect();
+
+
+        $link = $crawler->filter('ul#list-archive > li > a')->html();
+
+        self::assertStringStartsWith('micro_orc.fbx', $link);
+
+        self::assertEquals(200, $client->getResponse()->getStatusCode());
+    }
 }
--- tests/Wapinet/Bundle/WebTestCaseWapinet.php
+++ tests/Wapinet/Bundle/WebTestCaseWapinet.php
@@ -6,5 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 abstract class WebTestCaseWapinet extends WebTestCase
 {
-
+    /**
+     * @return string
+     */
+    protected function getFixturesPath()
+    {
+        return \dirname(static::$kernel->getRootDir()).'/tests/fixtures';
+    }
 }
