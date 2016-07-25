9a05543e81707b77dc276e7d7dfa3fe8fd9f80d0
Gemorroj <wapinet@gmail.com>
2016-07-25 16:10:59 +0300

    js код вынесен в отдельный файл

--- src/Wapinet/Bundle/Resources/public/js/core.js
+++ src/Wapinet/Bundle/Resources/public/js/core.js
@@ -147,3 +147,288 @@ const Vk = {
         return 'No data';
     }
 };
+
+
+var $document = $(document);
+
+
+// яндекс метрика
+(function (d, w, c) {
+    (w[c] = w[c] || []).push(function() {
+        try {
+            w.yaCounter26376855 = new Ya.Metrika({
+                id: 26376855,
+                clickmap: true,
+                trackLinks: true,
+                accurateTrackBounce: true
+            });
+        } catch(e) { }
+    });
+})(document, window, "yandex_metrika_callbacks");
+
+
+// jplayer
+window.Jplayer = {
+    'swfPath': "//cdnjs.cloudflare.com/ajax/libs/jplayer/2.9.2/jplayer/jquery.jplayer.swf"
+};
+
+
+// hypercomments
+var _commentsLoader = function ($pageContainer, xid) {
+    var $commentsContainer = $pageContainer.find("#hypercomments_widget");
+    $commentsContainer.empty();
+
+    var hcInterval = window.setInterval(function () {
+        if ("HC" in window) {
+            window.clearInterval(hcInterval);
+            var title = $pageContainer.find('#header-title').html();
+
+            HC.widget("Stream", {
+                "xid": xid,
+                "widget_id": 76882,
+                "title": title,
+                "href": window.location.pathname,
+                "like_href": window.location.href,
+                "like_title": title,
+                "append": $commentsContainer[0]
+            });
+        }
+    }, 100);
+};
+
+$document.on("pageshow", "#file_view", function () {
+    _commentsLoader(
+        $(this),
+        "file-" + window.location.pathname.split("/").reverse()[0]
+    );
+}).on("pageshow", "#gist_view", function () {
+    _commentsLoader(
+        $(this),
+        "gist-" + window.location.pathname.split("/").reverse()[0]
+    );
+}).on("pageshow", "#news_show", function () {
+    _commentsLoader(
+        $(this),
+        "news-" + window.location.pathname.split("/").reverse()[0]
+    );
+});
+
+$document.one("pagecreate", "#file_index", function () {
+    console.log('page create');
+    var $commentsContainer = $(":mobile-pagecontainer").find("#hypercomments_mix");
+    $commentsContainer.empty();
+
+    var hcInterval = window.setInterval(function () {
+        if ("HC" in window) {
+            window.clearInterval(hcInterval);
+            HC.widget("Mixstream", {
+                "widget_id": 76882,
+                "filter": "last",
+                "limit": 5,
+                "append": $commentsContainer[0]
+            });
+        }
+    }, 100);
+});
+
+
+
+$document.one("pagecreate", "#file_upload", function () {
+    FileUpload.pageCreate();
+});
+$document.one("pagecreate", "#file_edit", function () {
+    FileEdit.pageCreate();
+});
+$document.one("pagecreate", "#file_view", function () {
+    FileView.pageCreate();
+}).on("pageshow", "#file_view", function () {
+    FileView.pageShow($(this));
+});
+$document.one("pagecreate", "#file_swiper", function () {
+    FileSwiper.pageCreate();
+});
+
+
+$document.one("pagecreate", "#archiver_edit", function () {
+    Archiver.pageCreate();
+});
+
+$document.one("pagecreate", "#php_obfuscator_index", function () {
+    PhpObfuscator.pageCreate();
+});
+
+$document.one("pagecreate", "#translate_index", function () {
+    Translate.pageCreate();
+});
+
+$document.one("pagecreate", "#rest_index", function () {
+    Rest.pageCreate();
+});
+
+$document.one("pagecreate", "#guestbook_index", function () {
+    Guestbook.pageCreate();
+});
+
+$document.one("pagecreate", "#gist_view", function () {
+    Gist.pageCreate();
+});
+
+$document.one("pagecreate", "#wapinet_message_inbox", function () {
+    Message.pageCreate();
+}).one("pagecreate", "#wapinet_message_sent", function () {
+    Message.pageCreate();
+}).one("pagecreate", "#wapinet_message_deleted", function () {
+    Message.pageCreate();
+});
+
+
+// vk в профиле пользователя
+$document.one("pagecreate", "#fos_user_profile_show", function () {
+    var vkId = $(":mobile-pagecontainer").find("#user-vk").data("id");
+
+    if (vkId) {
+        $.getScript('https://api.vk.com/method/users.get?callback=Vk.show&fields=online,photo_200_orig&user_ids=' + vkId);
+    }
+});
+
+
+// подгрузка картинок в попапах
+$document.on("click", "a[href^='#image-']", function () {
+    $.mobile.loading("show");
+
+    var popup = $(this.getAttribute('href'));
+    var img = popup.find("img");
+
+    var src = img.attr('src');
+    var srcData = img.data('src');
+
+    if (src === srcData) {
+        popup.popup("open");
+        $.mobile.loading("hide");
+    } else {
+        img.load(function () {
+            popup.popup("open");
+            $.mobile.loading("hide");
+        });
+
+        img.attr('src', srcData);
+    }
+
+    return false;
+});
+
+
+// свайпер в пагинации
+$document.on("click", ".pagerfanta a", function () {
+    var $pageContainer = $(":mobile-pagecontainer");
+    var that = this;
+    var $this = $(that);
+
+    var swiperNext = function () {
+        $pageContainer.pagecontainer("change", $this.attr('href'), {
+            "transition": "slide"
+        });
+        return false;
+    };
+    var swiperPrev = function () {
+        $pageContainer.pagecontainer("change", $this.attr('href'), {
+            "transition": "slide",
+            "reverse": true
+        });
+        return false;
+    };
+
+
+    if ($this.hasClass("ui-first-child")) {
+        return swiperPrev();
+    }
+    if ($this.hasClass("ui-last-child")) {
+        return swiperNext();
+    }
+
+    var Page = {
+        "all": 0,
+        "current": 0,
+        "click": 0
+    };
+    $this.closest("div").find("a").not(".ui-first-child, .ui-last-child").each(function () {
+        Page.all++;
+
+        if (that === this) {
+            Page.click = Page.all;
+        }
+        if ($(this).hasClass("page-current")) {
+            Page.current = Page.all;
+        }
+    });
+
+    if (Page.click > Page.current) {
+        return swiperNext();
+    }
+    if (Page.click < Page.current) {
+        return swiperPrev();
+    }
+});
+
+
+// предпросмотр картинок в загружаемых файлах
+$document.on("change", 'input[type="file"]', function (e) {
+    var fileElement = e.target;
+    // только если поддерживается
+    if (fileElement.files) {
+        var file = fileElement.files[0];
+        Loader.previewCleaner(fileElement);
+        Loader.readFile(file, function (e) {
+            Loader.preview(e, file, fileElement);
+        });
+
+    }
+});
+
+
+// обновление капчи
+$document.on("click", 'a.captcha-reload', function () {
+    var $this = $(this);
+
+    var img = $(":mobile-pagecontainer").find('#' + $this.data('id'))[0];
+    img.src = $this.data('path') + '?n=' + (new Date()).getTime();
+});
+
+
+// выключение взаимозаменяющих полей в url_file
+$document.on("change", "fieldset.file-url input[type='file']", function (e) {
+    var state = 'enable';
+    if (e.target.value) {
+        state = 'disable';
+    }
+    $(":mobile-pagecontainer").find('fieldset.file-url input[type="url"]').textinput(state);
+}).on("change", "fieldset.file-url input[type='url']", function (e) {
+    var state = 'enable';
+    if (e.target.value) {
+        state = 'disable';
+    }
+    $(":mobile-pagecontainer").find('fieldset.file-url input[type="file"]').textinput(state);
+});
+
+
+// лоадер на ajax запросах
+$document.ajaxError(function () {
+    $.mobile.loading("show", {
+        html: '<h1 class="red">Ошибка</h1>',
+        textVisible: true,
+        textonly: true
+    });
+    setTimeout(function () {
+        $.mobile.loading('hide');
+    }, 5000);
+}).ajaxStart(function () {
+    $.mobile.loading('show');
+}).ajaxSuccess(function (data) {
+    // заголовок
+    var $title = $(data).find('title');
+    if ($title.length) {
+        Templating.setTitle($title.html()).render();
+    }
+
+    $.mobile.loading('hide');
+});
