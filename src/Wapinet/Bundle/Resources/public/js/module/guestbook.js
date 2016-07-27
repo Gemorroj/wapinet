7aaa050353c0bd0d4dd638586b55353d681925d5
Gemorroj <wapinet@gmail.com>
2016-07-27 09:34:46 +0300

    исправлены попапы и цитирование в гостевой

--- src/Wapinet/Bundle/Resources/public/js/module/guestbook.js
+++ src/Wapinet/Bundle/Resources/public/js/module/guestbook.js
@@ -4,17 +4,12 @@ const Guestbook = {
     pageCreate: function () {
         var $pageContainer = $(":mobile-pagecontainer");
 
-        var $textarea = $pageContainer.find("#message_form_message");
-        var $form = $pageContainer.find("form[name='message_form']");
-
-        $form.submit(function () {
-            $form.find("#message_form_submit").prop('disabled', true);
-        });
-
         var quote = function () {
+            var $link = $(this);
+            var $textarea = $pageContainer.find("textarea[name='message_form[message]']").filter(":visible");
             $textarea.focus();
 
-            var $liComment = $(this).parent().next();
+            var $liComment = $link.parent().next();
             var text = $liComment.children('div').text();
             var author = $liComment.find('span.comment-authorname>a').text();
             $textarea.val('[quote=' + author + ']' + text + '[/quote]');
@@ -24,12 +19,14 @@ const Guestbook = {
         };
         var popup = function () {
             var $link = $(this);
-            $($link.attr('href')).popup("open", {"transition": "turn", "positionTo": $link});
+            var selector = $link.attr('href');
+
+            $pageContainer.find(selector).popup("open", {"transition": "turn", "positionTo": $link});
 
             return false;
         };
 
-        $pageContainer.find('a[href^="#popup-"]').click(popup);
-        $pageContainer.find("a.item-quote").click(quote);
+        $pageContainer.on("click", "a[href^='#popup-']", popup);
+        $pageContainer.on("click", "a.item-quote", quote);
     }
 };
