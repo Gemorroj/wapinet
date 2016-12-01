101b8b8d55270c925e0d19ce8cc05c4a038147f2
Gemorroj <yD6n8jBAPru>
2016-12-01 15:49:08 +0300

    upd symfony to 3.2

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -154,10 +154,6 @@ class AppKernel extends Kernel
         return $bundles;
     }
 
-
-    /**
-     * {@inheritdoc}
-     */
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
         $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
