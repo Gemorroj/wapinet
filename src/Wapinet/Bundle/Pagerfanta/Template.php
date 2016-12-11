ae730bdc82fbc199948e01e65012cec1e0b06c34
Gemorroj <yD6n8jBAPru>
2016-12-11 10:12:24 +0300

    upgrade for new pagerfanta template

--- src/Wapinet/Bundle/Pagerfanta/Template.php
+++ src/Wapinet/Bundle/Pagerfanta/Template.php
@@ -15,6 +15,8 @@ class Template extends DefaultTemplate
         'dots_text'          => '...',
         'container_template' => '<nav data-role="controlgroup" data-type="horizontal">%pages%</nav>',
         'page_template'      => '<a href="%href%" data-role="button">%text%</a>',
-        'span_template'      => '<a href="#" class="ui-disabled %class%" data-role="button">%text%</a>'
+        'span_template'      => '<a href="#" class="ui-disabled %class%" data-role="button">%text%</a>',
+        'rel_previous'       => 'prev',
+        'rel_next'           => 'next'
     );
 }
