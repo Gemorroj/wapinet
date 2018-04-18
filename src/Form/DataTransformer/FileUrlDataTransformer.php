89c5a666767d519af84a9472378115b0f24930d1
Gemorroj <wapinet@mail.ru>
2018-04-18 17:39:08 +0300

    some updates

--- src/Form/DataTransformer/FileUrlDataTransformer.php
+++ src/Form/DataTransformer/FileUrlDataTransformer.php
@@ -152,50 +152,25 @@ class FileUrlDataTransformer implements DataTransformerInterface
     {
         $contentDisposition = $headers->get('Content-Disposition');
         if ($contentDisposition) {
-            $tmpName = Part::getHeaderOptions($contentDisposition);
-            if (isset($tmpName['filename*']) && $tmpName['filename*']) {
-                $encodedFilename = $this->decodeRfc5987($tmpName['filename*']);
-                if ($encodedFilename) {
-                    return $encodedFilename;
-                }
-            }
-            if (isset($tmpName['filename']) && $tmpName['filename']) {
-                return $tmpName['filename'];
+            $tmpName = Part::getHeaderOption($contentDisposition, 'filename');
+            if ($tmpName) {
+                return $tmpName;
             }
         }
 
-        $path = \parse_url($url, PHP_URL_PATH);
+        $path = \parse_url($url, \PHP_URL_PATH);
         if (null !== $path && '/' !== $path) {
             return $path;
         }
 
         $location = $headers->get('Location');
         if ($location) {
-            $locationPath = \parse_url($location, PHP_URL_PATH);
+            $locationPath = \parse_url($location, \PHP_URL_PATH);
             if (null !== $locationPath && '/' !== $locationPath) {
                 return $locationPath;
             }
         }
 
         return $default;
-    }
-
-
-    /**
-     * Decodes filenames given in the content-disposition header according
-     * to RFC5987, such as filename*=utf-8''filename.png. Note that the
-     * language sub-component is defined in RFC5646, and that the filename
-     * is URL encoded (in the charset specified)
-     *
-     * @see https://github.com/osTicket/osTicket/blob/v1.10.2/include/class.format.php#L70
-     * @param string $encodedFilename
-     * @return string|null
-     */
-    private function decodeRfc5987($encodedFilename): ?string
-    {
-        if (\preg_match("/(?P<charset>[\w!#$%&+^_`{}~-]+)'(?P<language>[\w-]*)'(?P<filename>.*)$/", $encodedFilename, $match)) {
-            return \mb_convert_encoding(\rawurldecode($match['filename']), 'UTF-8', $match['charset']);
-        }
-        return null;
     }
 }
