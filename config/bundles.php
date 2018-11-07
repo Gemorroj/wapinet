261701ae48523276d76c8d83f06b070bc5c24b7e
Gemorroj <wapinet@mail.ru>
2018-11-07 10:57:58 +0300

    upd webpack-encore & symfony

--- config/bundles.php
+++ config/bundles.php
@@ -21,4 +21,5 @@ return [
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Gregwar\CaptchaBundle\GregwarCaptchaBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
-var Encore = require('@symfony/webpack-encore');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
+const Encore = require('@symfony/webpack-encore');
 
 Encore
     // the project directory where compiled assets will be stored
     .setOutputPath('public/build/')
     // the public path used by the web server to access the previous directory
     .setPublicPath('/build')
+    .setManifestKeyPrefix('build/')
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
     // uncomment to create hashed filenames (e.g. app.abc123.css)
     // .enableVersioning(Encore.isProduction())
 
     // uncomment to define the assets of the project
-    .addEntry('js/app', [
+    .addEntry('app', [
         './assets/js/core.js',
         './assets/js/module/translate.js',
         './assets/js/module/php_obfuscator.js',
@@ -23,9 +23,16 @@ Encore
         './assets/js/module/file/edit.js',
         './assets/js/module/file/swiper.js',
         './assets/js/module/file/upload.js',
-        './assets/js/module/file/view.js'
+        './assets/js/module/file/view.js',
+        './assets/css/core.css'
     ])
-    .addStyleEntry('css/app', './assets/css/core.css')
+    .splitEntryChunks()
+    .copyFiles({
+        from: './assets/resources/',
+        to: 'resources/[path][name].[ext]',
+        // only copy files matching this pattern
+        //pattern: /\.(png|jpg|jpeg)$/
+    })
 
     // uncomment if you use Sass/SCSS files
     // .enableSassLoader()
@@ -34,12 +41,4 @@ Encore
     // .autoProvidejQuery()createdSharedEntry
 ;
 
-var config = Encore.getWebpackConfig();
-
-// копируем файлы которые не используются в js/css, а напрямую в twig
-config.plugins.push(new CopyWebpackPlugin([
-    { from: './assets/resources/', to: 'resources'}
-]));
-
-
-module.exports = config;
+module.exports = Encore.getWebpackConfig();
