19d0dd227245f281c28b04f7e04bfb14adef04d4
Gemorroj <wapinet@mail.ru>
2020-03-27 15:38:59 +0300

    upd deps

--- object-manager.php
+++ object-manager.php
@@ -4,7 +4,7 @@
 
 use App\Kernel;
 
-require \dirname(__DIR__).'/config/bootstrap.php';
+require __DIR__.'/config/bootstrap.php';
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $kernel->boot();
 
