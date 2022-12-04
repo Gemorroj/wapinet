f70ef74b8bf9cacbb8c02c9ac6a8b5c8645ec475
Gemorroj <wapinet@mail.ru>
2022-12-04 22:46:31 +0300

    fix: file->user not persist

--- src/Entity/File.php
+++ src/Entity/File.php
@@ -86,7 +86,7 @@ class File implements PasswordAuthenticatedUserInterface
      */
     private Collection $tags;
 
-    #[ORM\ManyToOne(targetEntity: User::class, cascade: ['persist'])]
+    #[ORM\ManyToOne(targetEntity: User::class)]
     #[ORM\JoinColumn(referencedColumnName: 'id', nullable: true, onDelete: 'CASCADE')]
     private ?User $user = null;
 
