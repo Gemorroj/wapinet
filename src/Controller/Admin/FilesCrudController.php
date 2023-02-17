ce253178feefffb0875e2a41d6d182063413bf49
Gemorroj <wapinet@mail.ru>
2023-02-17 11:40:03 +0300

    fix: set null for non password files

--- src/Controller/Admin/FilesCrudController.php
+++ src/Controller/Admin/FilesCrudController.php
@@ -4,10 +4,16 @@ namespace App\Controller\Admin;
 
 use App\Entity\File;
 use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Config\KeyValueStore;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use EasyCorp\Bundle\EasyAdminBundle\Event\AfterCrudActionEvent;
+use EasyCorp\Bundle\EasyAdminBundle\Event\AfterEntityUpdatedEvent;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityUpdatedEvent;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
@@ -16,6 +22,8 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
+use EasyCorp\Bundle\EasyAdminBundle\Orm\EntityUpdater;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class FilesCrudController extends AbstractCrudController
 {
@@ -81,6 +89,41 @@ class FilesCrudController extends AbstractCrudController
         }
 
         return [];
+    }
+
+    /**
+     * replace false fo null.
+     */
+    protected function ajaxEdit(EntityDto $entityDto, ?string $propertyName, bool $newValue): AfterCrudActionEvent
+    {
+        if ('password' === $propertyName) {
+            if ($newValue) {
+                throw new BadRequestHttpException();
+            }
+            $newValue = null;
+        }
+
+        $this->container->get(EntityUpdater::class)->updateProperty($entityDto, $propertyName, $newValue);
+
+        $event = new BeforeEntityUpdatedEvent($entityDto->getInstance());
+        $this->container->get('event_dispatcher')->dispatch($event);
+        $entityInstance = $event->getEntityInstance();
+
+        $this->updateEntity($this->container->get('doctrine')->getManagerForClass($entityDto->getFqcn()), $entityInstance);
+
+        $this->container->get('event_dispatcher')->dispatch(new AfterEntityUpdatedEvent($entityInstance));
+
+        $entityDto->setInstance($entityInstance);
+
+        $parameters = KeyValueStore::new([
+            'action' => Action::EDIT,
+            'entity' => $entityDto,
+        ]);
+
+        $event = new AfterCrudActionEvent($this->getContext(), $parameters);
+        $this->container->get('event_dispatcher')->dispatch($event);
+
+        return $event;
     }
 
     public function deleteEntity(EntityManagerInterface $entityManager, $entityInstance): void
